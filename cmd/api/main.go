bfb799c6642501d4906a4ced57b18baab171755a
Elvin Ega <proega3@gmail.com>
2025-02-11 16:27:41 +0300

    main go file

---
+++ cmd/api/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"socialmedia/internal/database"
+	"socialmedia/server"
+)
+
+func main() {
+	// Load environment variables
+	if err := godotenv.Load("../env/.env"); err != nil {
+		panic("Failed to load environment variables")
+	}
+
+	// Initialize database
+	db, err := gorm.Open(sqlite.Open("socialmedia.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	// Migrate database schema
+	db.AutoMigrate(
+		&database.User{},
+		&database.Post{},
+		&database.Like{},
+		&database.Comment{},
+		&database.EmailVerificationToken{},
+		&database.RevokedToken{},
+	)
+
+	// Initialize Fiber app
+	app := fiber.New(fiber.Config{
+		ServerHeader: "Fiber-SocialMedia",
+	})
+
+	// Middleware
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, Cookie",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowCredentials: true,
+	}))
+
+	// Routes
+	server.SetupRoutes(app, db)
+
+	// Start server
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	app.Listen(":" + port)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ElvinEga/socialmedia
+
+go 1.23.4
