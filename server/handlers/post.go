262cef85d425f839773557c89c8bb9c179cfb4aa
Elvin Ega <proega3@gmail.com>
2025-02-11 16:33:47 +0300

    post handler

---
+++ server/handlers/post.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func CreatePost(c *fiber.Ctx) error {
+	// Implementation omitted for brevity
+}
+
+func GetTimeline(c *fiber.Ctx) error {
+	// Implementation omitted for brevity
+}
