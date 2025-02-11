3c169312fb3fd1dff6f11fa20466ccfc7ba9419d
Elvin Ega <proega3@gmail.com>
2025-02-11 16:32:40 +0300

    handler user

---
+++ server/handlers/user.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func UpdateProfile(c *fiber.Ctx) error {
+	// Implementation omitted for brevity
+}
+
+func ChangePassword(c *fiber.Ctx) error {
+	// Implementation omitted for brevity
+}
